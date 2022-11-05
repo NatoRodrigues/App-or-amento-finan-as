200bdb8b4308d3af8b291c853bad3562f07a3a54
NatoRodrigues <renwise2.0@gmail.com>
2022-11-05 19:06:08 -0300

    Correções

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-    class Despesa {
-        constructor (ano, mes, dia, tipo, descricao, valor) {
-            this.ano = ano
-            this.mes = mes
-            this.dia = dia
-            this.tipo = tipo
-            this.descricao = descricao
-            this.valor = valor
-        }
-    }
+class Despesa {
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+}
 
     class Bd {
 
