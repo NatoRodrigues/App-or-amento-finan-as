d6efa8428fe549b8c8eda6f99949040211930766
NatoRodrigues <renwise2.0@gmail.com>
2022-11-05 19:02:32 -0300

    organizando funções

--- app.js
+++ app.js
@@ -35,22 +35,22 @@
     
     let bd = new Bd()
 
-    function cadastrarDespesa(){
-        
-    let ano = document.getElementById('ano')
-    let mes = document.getElementById('mes')
-    let dia = document.getElementById('dia')
-    let tipo = document.getElementById('tipo')
-    let descricao = document.getElementById('descricao')
-    let valor = document.getElementById('valor')
-    let despesa = new Despesa(
-                                ano.value, 
-                                mes.value, 
-                                dia.value, 
-                                tipo.value, 
-                                descricao.value, 
-                                valor.value
-                                )
+    function cadastrarDespesa() {
 
+        let ano = document.getElementById('ano')
+        let mes = document.getElementById('mes')
+        let dia = document.getElementById('dia')
+        let tipo = document.getElementById('tipo')
+        let descricao = document.getElementById('descricao')
+        let valor = document.getElementById('valor')
+        let despesa = new Despesa(
+                        ano.value, 
+                        mes.value,
+                        dia.value, 
+                        tipo.value, 
+                        descricao.value,
+                        valor.value
+        )
+    
         bd.gravar(despesa)
     }
