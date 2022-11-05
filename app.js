514da15d43a5732490271c56636b7bb4ab4ffba9
NatoRodrigues <renwise2.0@gmail.com>
2022-11-05 19:29:36 -0300

    implementando código

--- app.js
+++ app.js
@@ -9,48 +9,50 @@ class Despesa {
 	}
 }
 
-    class Bd {
+class Bd {
 
-        constructor() {
-            let id = localStorage.getItem('id')
-    
-            if(id === null) {
-                localStorage.setItem('id', 0)
-            }
-        }
-    
-        getProximoId() {
-            let proximoId = localStorage.getItem('id')
-            return parseInt(proximoId) + 1
-        }
-    
-        gravar(d) {
-            let id = this.getProximoId()
-    
-            localStorage.setItem(id, JSON.stringify(d))
-    
-            localStorage.setItem('id', id)
-        }
-    }
-    
-    let bd = new Bd()
+	constructor() {
+		let id = localStorage.getItem('id')
 
-    function cadastrarDespesa() {
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
 
-        let ano = document.getElementById('ano')
-        let mes = document.getElementById('mes')
-        let dia = document.getElementById('dia')
-        let tipo = document.getElementById('tipo')
-        let descricao = document.getElementById('descricao')
-        let valor = document.getElementById('valor')
-        let despesa = new Despesa(
-                        ano.value, 
-                        mes.value,
-                        dia.value, 
-                        tipo.value, 
-                        descricao.value,
-                        valor.value
-        )
-    
-        bd.gravar(despesa)
-    }
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+
+	gravar(d) {
+		let id = this.getProximoId()
+
+		localStorage.setItem(id, JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
+}
+
+let bd = new Bd()
+
+
+function cadastrarDespesa() {
+
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
+
+	bd.gravar(despesa)
+}
